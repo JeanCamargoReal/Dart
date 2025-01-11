ee733c2cb6eead437baa002067c231881c297209
Jean Camargo <jeancamargoreal@icloud.com>
2025-01-11 19:02:23 -0300

    tipos dinamicos

---
+++ funcao/dinamico.dart
@@ -0,0 +1,11 @@
+void main() {
+  juntar(1, 9);
+  juntar("Bom", " dia");
+  juntar("Eu tenho esse valor em dinheiro = ", 1700000);
+}
+
+juntar(a, b) {
+  print(a.toString() + b.toString());
+
+  return a.toString() + b.toString();
+}
